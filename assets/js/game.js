a036de79e874e6cf20c541f20400431f3c86b7eb
Cooper <cooperflynn@utexas.edu>
2020-12-17 17:14:31 -0600

    add while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,9 +17,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    while(enemyHealth> 0) {
+       //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -75,11 +74,17 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 
     // Log a resulting message to the console so we know that it worked.
 
+    }
+    
+   
+
 
     
 };
 
 
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth= 50;
+  fight(pickedEnemyName);
 }
