b4189586f5cc1cc2f33a5d5c26aa37e8e2ce05ce
Cooper <cooperflynn@utexas.edu>
2020-12-26 18:56:06 -0600

    fixed case sensitivity

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,26 +4,38 @@
 //  *Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
+var fightOrSkip = function() {
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip") {
+    var confirmSkip= window.confirm("Are you sure you'd like to quit?");
+
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      playerInfo.playerMoney = Math.max(0, playerInfo.playerMoney - 10);
+      
+      return true;
+    }
+  }
+  return false;
+}
 
 var fight = function(enemy) {
   console.log(enemy);
 
   while(enemy.health > 0 && playerInfo.health > 0) {
- 
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
- if (promptFight === "skip" || promptFight === "SKIP") {
-      
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-        if (confirmSkip) {
-        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-       
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money" , playerInfo.money)
-        break;
+    if (fightOrSkip()) {
+    // if true, leave fight by breaking loop
+      break;
     }
-  }
+
     var damage = randomNumber(playerInfo.attack -3, playerInfo.attack)
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
